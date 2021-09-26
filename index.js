b868ffa743e3e101f49d8042641f5df1457c14d9
seok <76927397+pds0309@users.noreply.github.com>
2021-09-26 10:10:10 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,21 @@
-const https = require('http');
-const convert = require('xml-js');
-const mom = require('moment');
+const AWS = require("aws-sdk");
+const docClient = new AWS.DynamoDB.DocumentClient();
+const tableName = 'covid-sms-user';
+ const scanTable = async (tableName) => {
+    const params = {
+        TableName: tableName,
+    };
+    const scanResults = [];
+    var items;
+    do{
+        items =  await docClient.scan(params).promise();
+        items.Items.forEach((item) => scanResults.push(item));
+        params.ExclusiveStartKey  = items.LastEvaluatedKey;
+    }while(typeof items.LastEvaluatedKey !== "undefined");
+    
+    return scanResults;
+
+};
 exports.handler = async (event) => {
-    let today = mom().format("YYYYMMDD");
-    let ysday = mom().subtract(1, 'd').format("YYYYMMDD");
-
-    let dataString = '';
-    let xmlToJson;
-    let js;
-    let papakey = process.env.PAPAKEY;
-    let reqq = "http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson?serviceKey="+papakey;
-    reqq = reqq + "&startCreateDt="+ysday +"&endCreateDt="+today;
-    const response = await new Promise((resolve, reject) => {
-         const req = https.get(reqq, function(res) {
-          res.on('data', chunk => {
-            dataString += chunk;
-            xmlToJson = convert.xml2json(dataString, {compact: true, spaces: 4});
-            js = JSON.parse(xmlToJson);
-          });
-
-          res.on('end', () => {
-            resolve({
-                statusCode: 200,
-                body: js.response.body.items.item,
-                yesterday: js.response.body.items.item[1]
-            });
-          });
-         });
-
-         req.on('error', (e) => {
-          reject({
-              statusCode: 500,
-              body: '"error"'
-          });
-         });
-    });
-  return response.body;
+    return  scanTable(tableName);
 };
